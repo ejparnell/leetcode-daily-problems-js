b6648840b180de1ae87b0abace8ad512881bda14
Elizabeth Parnell <elizabethprnll@gmail.com>
2024-01-29 00:00:00 +0000

    Complete on day: Jan 29

---
+++ day-19.js
@@ -0,0 +1,9 @@
+const pivotIndex = nums => {
+    const totalSum = nums.reduce((sum, num) => sum + num, 0)
+    let leftSum = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (leftSum === totalSum - leftSum - nums[i]) return i
+        leftSum += nums[i]
+    }
+    return -1
+}
