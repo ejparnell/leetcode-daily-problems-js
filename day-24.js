2c2f105d5058f872b4b814d7ec7c74313cd812ea
Elizabeth Parnell <elizabethprnll@gmail.com>
2024-02-05 00:00:00 +0000

    Complete on day: Feb 05

---
+++ day-24.js
@@ -0,0 +1,8 @@
+const removeStars = s => {
+    const stack = []
+    for (let char of s) {
+        if (char === '*') stack.pop()
+        else stack.push(char)
+    }
+    return stack.join('')
+}
