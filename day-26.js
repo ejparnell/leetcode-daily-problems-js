46506e5b146077607df165f6d2f401e4e02bba6a
Elizabeth Parnell <elizabethprnll@gmail.com>
2024-02-07 00:00:00 +0000

    Complete on day: Feb 07

---
+++ day-26.js
@@ -0,0 +1,20 @@
+const decodeString = s => {
+    const stack = []
+    let currentNum = 0
+    let currentStr = ''
+    for (let char of s) {
+        if (!isNaN(char)) {
+            currentNum = currentNum * 10 + parseInt(char)
+        } else if (char === '[') {
+            stack.push([currentStr, currentNum])
+            currentStr = ''
+            currentNum = 0
+        } else if (char === ']') {
+            const [prevStr, num] = stack.pop()
+            currentStr = prevStr + currentStr.repeat(num)
+        } else {
+            currentStr += char
+        }
+    }
+    return currentStr
+}
