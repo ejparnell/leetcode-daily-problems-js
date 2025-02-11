88037ad7df24d67d93bc8d281c74ca119e59ae1a
Elizabeth Parnell <elizabethprnll@gmail.com>
2025-02-11 09:25:20 -0500

    two

--- feb/day-11.js
+++ feb/day-11.js
@@ -16,3 +16,41 @@ var longestCommonPrefix = function(strs) {
     }
     return prefix
 };
+
+var threeSum = function (nums) {
+    nums =  nums.sort((a, b) => a - b)
+
+    const results = []
+    for (let i = 0; i <= nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue
+        }
+
+        let left = i + 1
+        let right = nums.length - 1
+
+        while (left < right) {
+            sum = nums[i] + nums[left] + nums[right]
+
+            if (sum === 0) {
+                results.push([nums[i], nums[left], nums[right]])
+
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left += 1
+                }
+
+                while (left < right && nums[right] === nums[right + 1]) {
+                    right -= 1
+                }
+
+                left += 1
+                right -= 1
+            } else if (sum < 0) {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+    }
+    return results
+};
