a31c0505c7c2710b7154e68941611942de36276e
Elizabeth Parnell <elizabethprnll@gmail.com>
2025-04-07 09:55:57 -0400

    Three

--- april/7.js
+++ april/7.js
@@ -20,3 +20,12 @@ var rotate = function(nums, k) {
         nums[i] = rotated[i]
     }
 };
+
+var containsDuplicate = function(nums) {
+    let seen = new Set;
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) return true
+        else seen.add(nums[i]);
+    }
+    return false
+};
